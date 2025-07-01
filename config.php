75290b5562cd82fcf8ffca88cade8fc3846042b0
Mpumi <142667245+MpumiNico@users.noreply.github.com>
2025-07-01 09:58:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
-// config.php
 
-// Change these values to match your local database settings
+
+
 $host = "localhost";
 $username = "root";
 $password = "";
 $database = "inventory_db";
 
-// Create connection
+
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
